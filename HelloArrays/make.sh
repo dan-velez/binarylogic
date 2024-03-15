dce49781d213b25d79e222a0c3e4739cbd3025ad
dan-velez <daniel.enr.velez@gmail.com>
2024-03-15 18:59:52 -0400

    Ignore compiled bytecode

--- HelloArrays/make.sh
+++ HelloArrays/make.sh
@@ -1,10 +1,10 @@
 # Compile JAR from root directory.
 javac -d ./classes/ $(find ./source/ -name "*.java")
-cd classes/ && jar -cvmf manifest.txt ../HelloArrays.jar com
+cd classes/ && jar -cvmf manifest.txt ../HelloArrays.jar $(find . -name "*.class")
 cd ../
 
 # Run main class file.
 java -cp classes com.blit.Main
 
 # Run JAR
-# java -jar app1.jar
+# java -jar HelloArrays.jar
--- JavaScaffold/README.md
+++ JavaScaffold/README.md
@@ -3,3 +3,6 @@ Creates boilerplate code for a Java project compiled and ran in the command
 line. Include a makefile, clean file, and main class creation.
 
 ## TODO ##
+* Version updating.
+* Create README.
+* Create .gitignore.
