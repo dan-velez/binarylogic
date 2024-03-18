a8d66828f402c12c071ea1922d9dc98b11063967
dan-velez <daniel.enr.velez@gmail.com>
2024-03-18 10:02:03 -0400

    Develop lexer

--- AlpacaCryptoBot/make.sh
+++ AlpacaCryptoBot/make.sh
@@ -1,5 +1,6 @@
 # Compile JAR from root directory.
-javac -d ./classes/ $(find ./source/ -name "*.java")
+# javac -d ./classes/ $(find ./source/ -name "*.java")
+javac -d classes/ -cp ./jars/javax.json-1.1.4.jar $(find ./source/ -name *.java)
 cd classes/ && jar -cvmf manifest.txt ../AlpacaCryptoBot.jar $(find . -name "*.class")
 cd ../
 
--- AlpacaCryptoBot/source/com/blit/AlpacaCryptoBot.java
+++ AlpacaCryptoBot/source/com/blit/AlpacaCryptoBot.java
@@ -3,13 +3,23 @@ package com.blit;
 import java.net.http.*;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.temporal.ChronoUnit;
 
 // import javax.json;
-//import org.glassfish.json;
-import org.glassfish.json.api.*;
+// import org.glassfish.json;
+// import javax.json.Json;
+// import org.glassfish.json.*;
+// import javax.json.JsonReader;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 
 public class AlpacaCryptoBot {
         
@@ -17,26 +27,18 @@ public class AlpacaCryptoBot {
     String apiSecret = "xzvxQcLOZ7nWpeXUiDjar9WVje8QuBHDfnrvDNJH";
     String apiEndpointType = "paper"; // || live.
     String apiDataType = "iex"; // must be either "iex" or "sip".
-    String endpoint = "https://paper-api.alpaca.markets/v2/account";
-    String endpointLive = "https://api.alpaca.markets/v2/account";
+    String endpoint = "https://paper-api.alpaca.markets/v2";
+    String endpointLive = "https://api.alpaca.markets/v2";
 
     public AlpacaCryptoBot () {
         System.out.println("Alpaca Crypto Bot v1.0.0");
         System.out.println("------------------------");
 
         try {
-            URI uri = new URI(endpoint);
-
-            System.out.println("Host: " + uri.getHost());
-            System.out.println("Port: " + uri.getPort());
-            System.out.println("Query: " + uri.getQuery());
-            System.out.println("Path: " + uri.getPath());
-            System.out.println("Raw Path: " + uri.getRawPath());
-            System.out.println("Scheme: " + uri.getScheme());
-            System.out.println("Resolve: " + uri.resolve(uri));
+            this.printURIInfo(endpoint+"/account");
             
             HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI(endpoint))
+                .uri(new URI(endpoint+"/account"))
                 .version(HttpClient.Version.HTTP_2)
                 .headers("APCA-API-KEY-ID", apiKey, 
                          "APCA-API-SECRET-KEY", apiSecret)
@@ -50,12 +52,43 @@ public class AlpacaCryptoBot {
                 .send(request, BodyHandlers.ofString());
             
             //System.out.println("Headers: " + resp.headers());
-            System.out.println("Code: " + resp.statusCode());
-            //System.out.println("Response: " + resp.body());
+            System.out.println("Status Code: " + resp.statusCode());
+            String jsonText = resp.body().toString();
+            //System.out.println(jsonText);
+            this.parseResp(jsonText);
         } catch (URISyntaxException e) {
             e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void parseResp (String resp) {
+        // JSONObject reader = new JSONObject();
+        // Json json = new Json();
+        // JsonReader reader = new JsonReader();
+        String fname = "/home/danny/binarylogic/AlpacaCryptoBot/output.json";
+        try {
+            InputStream fis = new FileInputStream(fname);
+            JsonReader jsonReader = Json.createReader(fis);
+        } catch (Exception e) {
+            System.out.println("Cannot open file: "+fname);
+            e.printStackTrace();
+        }
+    }
+
+    private void printURIInfo (String uriText) {
+        try {
+            URI uri = new URI(uriText);
+            System.out.println("Host: " + uri.getHost());
+            System.out.println("Port: " + uri.getPort());
+            System.out.println("Query: " + uri.getQuery());
+            System.out.println("Path: " + uri.getPath());
+            System.out.println("Raw Path: " + uri.getRawPath());
+            System.out.println("Scheme: " + uri.getScheme());
+            System.out.println("Resolve: " + uri.resolve(uri));
+        } catch (Exception e) {
+            System.out.println("Cannot create URI: "+uriText);
         }
     }
 }
